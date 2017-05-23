2665f8290d5d604374433c9f78d05c4f0a400256
damiankamela <damian.kamela+github@gmail.com>
2017-05-23 20:36:17 +0200

    changed Connector

--- src/Kamde/StackExtBundle/Service/ApiClient/Connector.php
+++ src/Kamde/StackExtBundle/Service/ApiClient/Connector.php
@@ -24,4 +24,33 @@ class Connector
         dump(\GuzzleHttp\json_encode($response->getBody(), true));
         die;
     }
+
+    /**
+     * @param       $url
+     * @param array $parameters
+     * @return string
+     */
+    protected function normalizeUrl(string $url, array $parameters = [])
+    {
+        $normalizedUrl = $url;
+        if (!empty($parameters)) {
+            $normalizedUrl .= (false !== strpos($url, '?') ? '&' : '?') . http_build_query($parameters, '', '&');
+        }
+
+        return $normalizedUrl;
+    }
+
+    /**
+     * @param string $method
+     * @param string $uri
+     * @param array  $options
+     * @return mixed
+     */
+    protected function getResponse(string $method, string $uri, array $options = [])
+    {
+        $response = $this->client->request($method, $uri, $options);
+
+        return \GuzzleHttp\json_decode($response->getBody(), true);
+    }
+
 }
