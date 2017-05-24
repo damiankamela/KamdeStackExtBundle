fdb99a60af60e808f5b702f09f74b281a81951d6
damiankamela <damian.kamela+github@gmail.com>
2017-05-24 21:50:05 +0200

    added tests

---
+++ src/Kamde/StackExtBundle/Tests/Service/ApiClient/Resource/AbstractResourceTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Kamde\StackExtBundle\Service\ApiClient\Resource;
+
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7\Request;
+use Kamde\StackExtBundle\Service\ApiClient\Connector;
+use PHPUnit_Framework_MockObject_MockObject as Mock;
+
+class AbstractResourceTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var AbstractResource */
+    protected $resource;
+
+    /** @var Connector|Mock */
+    protected $connectorMock;
+
+    public function setUp()
+    {
+        $this->connectorMock = $this->getMockBuilder(Connector::class)->disableOriginalConstructor()->getMock();
+
+        $this->resource = $this->getMockBuilder(AbstractResource::class)
+            ->setConstructorArgs([$this->connectorMock])->getMockForAbstractClass();
+    }
+
+    /**
+     * @test
+     * @expectedException \Kamde\StackExtBundle\Service\ApiClient\Exception\ResourceNotSetException
+     */
+    public function not_set_id_should_throw_error()
+    {
+        $this->resource->getTest();
+    }
+
+    /**
+     * @test
+     * @expectedException \Kamde\StackExtBundle\Service\ApiClient\Exception\MethodNotFoundException
+     */
+    public function calling_invalid_method_should_throw_error()
+    {
+        $this->resource->setId(1);
+
+        $this->resource->test();
+    }
+
+    /**
+     * @test
+     * @expectedException \Kamde\StackExtBundle\Service\ApiClient\Exception\MethodNotFoundException
+     */
+    public function calling_unavailable_resource_should_throw_error()
+    {
+        $this->resource->setId(1);
+
+        $this->connectorMock
+            ->expects(self::once())
+            ->method('getResponse')
+            ->willThrowException(new RequestException('foo', new Request('GET', 'foo')));
+
+        $this->resource->getTest();
+    }
+
+    /**
+     * @test
+     */
+    public function should_retrieve_resource_data()
+    {
+        $this->resource->setId(1);
+
+        $this->connectorMock
+            ->expects(self::once())
+            ->method('getResponse')
+            ->with('GET', 's/1/foo-bar')
+            ->willReturn(['foo' => 'bar']);
+
+        $response = $this->resource->getFooBar();
+
+        $this->assertEquals(['foo' => 'bar'], $response);
+    }
+}
